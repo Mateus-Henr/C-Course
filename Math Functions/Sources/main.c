a11094d5dd5027fc7bf5775ba25b348a78bd7670
Mateus-Henr <mateush.study@gmail.com>
2022-01-14 19:34:33 -0300

    Math header file

---
+++ Math Functions/Sources/main.c
@@ -0,0 +1,28 @@
+/*  Author: Mateus Henrique
+    Purpose: This program shows functions from "math.h" header file.
+*/
+
+/*
+    Math functions (<math.h>)
+    acosh() - Retunrs the hyperbolic arccosine of x.
+    asin() - Returns the arcsine of x as an angle.
+    atan() - Returns the arctangent as an angle.
+    cos() - Returns the consine of x.
+    sin() - Returns the consine of x.
+    tan() - Returns the tangent of x.
+    ceil() - Rounds x to the closest biggest integer value.
+    floor() - Rounds x to the closest smallest integer value.
+    log() - Returns the natural log of x.
+    nan() - returns a NaN. Tells if it's a number.
+    pow() - Returns x raised to some power.
+    sqrt() - Returns the square root of x.
+    remainder() - Returns the remainder of x divided by y.
+    round() - Rounds to the nearest integer.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
