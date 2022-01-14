1bc9f77739aaa7f40c35840306025656a863a58a
Mateus-Henr <mateush.study@gmail.com>
2022-01-14 20:08:41 -0300

    Further topics to study

---
+++ Further Topics To Study/Sources/main.c
@@ -0,0 +1,76 @@
+/*  Author: Mateus Henrique
+    Purpose: This program describes some futher topics to study.
+*/
+
+/*
+    Defining your own tpes (typedef)
+    Similar to when defining an enum.
+
+    More on preprocessors
+    String concatenation. Used for optimization.
+
+    Void *
+    Used for flexibility.
+
+    Static libraries and shared objects
+    Creating your own libraries and objects.
+
+    Macros
+    Run by the preprocessors.
+
+    Unions
+    Having a multiple type of data returned. A member being an int and a float.
+
+    Function pointers
+    Pointers that points to functions.
+    
+    Advanced pointers
+    Double pointers, etc.
+
+    Variable arguments to functions (variadic functions)
+    Creating your own.
+
+    Dynamic linking (dlm_open)
+    Opening up libraries at runtime. Better performance.
+
+    Signals, forking and inter-process communication
+    Programs communicating via files, sockets, etc.
+
+    Threading and concurrency
+    Multiple execution points in the program.
+    
+    Sockets
+    Used for networking. Getting data from another computer.
+
+    setjmp and lonjmp
+    Restoring state.
+
+    More on memory management and fragmentation
+    Manage memory more wisely avoiding fragmentations.
+
+    More on making a program portable
+    Allows the program to be run in various OSs.
+
+    Interfacing wwith kernel modules (drivers and ioctls)
+    Interactions with this part of the OS.
+
+    More on compiler and linker flags
+    Make the program more efficient. "-Wall" was an example.
+
+    Advanced use of fdb
+    Using the debugger in a more advanced way.
+
+    Profiling and tracing tools (gprof, dtrace, strace)
+    Tools that analyse your code to see if there are any bad programming practises,
+    dead code, deadlocks, etc.
+
+    Memory debugging tools such as valgrind
+    Used when you have problems in memory.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
